0eff512946c89986291ab0b6321cb854629af39f
Nfer Zhuang <nfer.zhuang@gmail.com>
2018-12-05 15:17:49 +0800

    add ignoreRootList config

--- book/plugin.js
+++ book/plugin.js
@@ -1,6 +1,12 @@
 require(["gitbook"], function(gitbook) {
     gitbook.events.bind("start", function(e, config) {
         var conf = config["baidu-nfer"] || {};
+        var root = gitbook.state.root;
+        var ignoreRootList = conf.ignoreRootList || [];
+        var ignore = ignoreRootList.some(item => root.indexOf(item) !== -1);
+        if (ignore) {
+            return;
+        }
         var hm = document.createElement('script');
         hm.src = 'https://hm.baidu.com/hm.js?' + conf.token;
         var s = document.getElementsByTagName('script')[0];
